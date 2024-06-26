856b86bb97eee366328815a875e69c5a01ce8202
Ratchapon Ngamdan <ratchapon.ngam@gmail.com>
2024-06-26 18:23:36 -0400

    removed something

--- tailwind.config.js
+++ tailwind.config.js
@@ -77,11 +77,6 @@ module.exports = {
         "dark-header": "#dab7f0",
         "dark-text": "#fbfbfb",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
